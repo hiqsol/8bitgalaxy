8a3aa20968bc011fb5d76b761421f5053ab2145e
Andrii Vasyliev <sol@hiqdev.com>
2022-12-26 17:01:56 +0200

    tune home alignment again

--- src/legacy/js/DivDrawer/HomeDrawer.js
+++ src/legacy/js/DivDrawer/HomeDrawer.js
@@ -27,19 +27,20 @@ class HomeDrawer extends aDrawer {
     let progressIndent = params.direction.isTopToBottom ? 31 : -46;
     e.setAttribute('race', home.player.race);
     if (params.direction.isTopToBottom) {
-      this._drawer.draw(e, home.progress, new Params(32*xStep,  0*yStep, Direction.LeftToRight));
-      this._drawer.draw(e, home.hand,     new Params(23*xStep, -2*yStep, Direction.TopToBottom));
+      this._drawer.draw(e, home.progress, new Params(33*xStep,  0*yStep, Direction.LeftToRight));
+      this._drawer.draw(e, home.hand,     new Params(24*xStep, -2*yStep, Direction.TopToBottom));
       this._drawer.draw(e, home.discard,  new Params(17*xStep,  9*yStep, params.direction));
       this._drawer.draw(e, home.reserve,  new Params(17*xStep,  0*yStep, params.direction));
+      this._drawer.draw(e, home.factory,  new Params( 8*xStep,  0*yStep, params.direction));
+      this._drawer.draw(e, home.research, new Params( 0*xStep,  0*yStep, params.direction));
     } else {
-      this._drawer.draw(e, home.progress, new Params(48*xStep,  0*yStep, Direction.LeftToRight));
-      this._drawer.draw(e, home.hand,     new Params(23*xStep,  0*yStep, Direction.TopToBottom));
+      this._drawer.draw(e, home.progress, new Params(49*xStep,  0*yStep, Direction.LeftToRight));
+      this._drawer.draw(e, home.hand,     new Params(24*xStep,  0*yStep, Direction.TopToBottom));
       this._drawer.draw(e, home.discard,  new Params(15*xStep,  9*yStep, params.direction));
       this._drawer.draw(e, home.reserve,  new Params(15*xStep,  0*yStep, params.direction));
+      this._drawer.draw(e, home.factory,  new Params( 8*xStep, -1*yStep, params.direction));
+      this._drawer.draw(e, home.research, new Params( 0*xStep, -1*yStep, params.direction));
     }
-
-    this._drawer.draw(e, home.factory,    new Params( 8*xStep,  0*yStep, params.direction));
-    this._drawer.draw(e, home.research,   new Params( 0*xStep,  0*yStep, params.direction));
   }
 
   drawAlien(e, home, params) {
