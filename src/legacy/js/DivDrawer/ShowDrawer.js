91ac2e8ec3d821a438c572dddeb9997db68036e6
Andrii Vasyliev <sol@hiqdev.com>
2023-01-02 22:12:36 +0200

    mostly implemented Show Card with `DivDrawer`

--- src/legacy/js/DivDrawer/ShowDrawer.js
+++ src/legacy/js/DivDrawer/ShowDrawer.js
@@ -1,12 +1,12 @@
+import {Div} from './DivDrawer.js';
 import Drawer from './Drawer.js';
 import Params from './Params.js';
 import aDrawer from './aDrawer.js';
-import Direction from './Direction.js';
 
 class ShowDrawer extends aDrawer {
   constructor(drawer) {
     super(drawer);
-    this._HTML = `<div id="Show"></div>`;
+    this._HTML = `<div class="Show" id="Show"></div>`;
   }
 
   draw(parent, board, params) {
@@ -16,15 +16,47 @@ class ShowDrawer extends aDrawer {
   show(card) {
     let show = this.elem('Show');
     show.innerHTML = '';
-    if (!card.classList.contains("Selected")) {
-      let copy = card.cloneNode(true);
-      copy.id = 'ShowCopy.' + card.id;
-      copy.style.left = null;
-      copy.style.top = null;
-      copy.style.bottom = '0px';
-      copy.classList.remove('Turned');
-      show.appendChild(copy);
+    if (card.classList.contains("Selected")) {
+      return;
     }
+    let copy = card.cloneNode(true);
+    copy.id = 'ShowCopy.' + card.id;
+    copy.draggable = false;
+    copy.style.top = null;
+    copy.style.left = '0px';
+    copy.style.bottom = '0px';
+    copy.classList.remove('Turned');
+    show.appendChild(copy);
+
+    this.drawNote(
+      copy.querySelector(".Normal .Spec.Level .Value"),
+      'Level', 3
+    );
+
+    this.drawNote(
+      copy.querySelector(".Image .Klass"),
+      'Type', 3, new Params(1.5, 1)
+    );
+
+    this.drawNote(
+      copy.querySelector(".Normal .Spec.Colonization .Value"),
+      'Power', 1
+    );
+
+    this.drawNote(
+      copy.querySelector(".Normal .Spec.Cooperation .Value"),
+      'More', 0.1
+    );
+  }
+
+  drawNote(parent, text, d, params = new Params().rt(0, 0)) {
+    if (!parent) return;
+    let y = -1*d;
+    let h = Math.abs(d);
+    let e = this.drawer.draw(parent, new Div('Note'), params);
+    this.drawer.draw(e, new Div('Line'), new Params((d-2)/4, y).wh(3, h));
+    this.drawer.draw(e, new Div('Text', text), new Params((h+2)/4, y-1));
+    return e;
   }
 }
 
