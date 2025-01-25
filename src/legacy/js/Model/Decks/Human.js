cb374496215506966c1e261f561ca8624455e144
Andrii Vasyliev <sol@hiqdev.com>
2025-01-25 21:48:36 +0100

    tune Human deck

--- src/legacy/js/Model/Decks/Human.js
+++ src/legacy/js/Model/Decks/Human.js
@@ -16,15 +16,15 @@ class Human {
       'Human-Ship-2p':      'a1c,p1p,c1p:extract',
       'Human-Ship-2s':      'a1p,p1s,c1s:explore',
 
-      'Human-Ship-3a':      'a4p,p1a,c1a',
-      'Human-Ship-3c':      'a4a,p1c,c1c',
-      'Human-Ship-3p':      'a4s,p1p,c1p',
-      'Human-Ship-3s':      'a4c,p1s,c1s',
+      'Human-Ship-3a':      'a4p,r1a,r2c,p2a,c1a',
+      'Human-Ship-3c':      'a4a,r1c,r2a,p2c,c1c',
+      'Human-Ship-3p':      'a4s,r1p,r2s,p2p,c1p',
+      'Human-Ship-3s':      'a4c,r1s,r2p,p2s,c1s',
 
-      'Human-Ship-4a':      'a3c,r2a,r2c,p2a,c1a:smuggle',
-      'Human-Ship-4c':      'a3a,r2c,r3a,p2c,c1c:improve',
-      'Human-Ship-4p':      'a3s,r2p,r2s,p2p,c1p:upgrade',
-      'Human-Ship-4s':      'a3p,r2s,r3p,p2s,c1s:exhaust',
+      'Human-Ship-4a':      'a3c,r2a,r1c,p2a,c1a:smuggle',
+      'Human-Ship-4c':      'a3a,r2c,r1a,p2c,c1c:improve',
+      'Human-Ship-4p':      'a3s,r2p,r1s,p2p,c1p:upgrade',
+      'Human-Ship-4s':      'a3p,r2s,r1p,p2s,c1s:exhaust',
 
       'Human-Ship-5a':      'a6s,r3a,r3s,p3a,c2a',
       'Human-Ship-5c':      'a6p,r3c,r4p,p2c,c2c',
@@ -47,15 +47,15 @@ class Human {
       'Human-Hero-2p':      'a2c,p2p,c2p:upgrade',
       'Human-Hero-2s':      'a2a,p2s,c2s:recycle',
 
-      'Human-Hero-3a':      'a3p,r1a,r1p,p3a,c3a:mastery',
-      'Human-Hero-3c':      'a3s,r1c,r1s,p3c,c3c:mastery',
-      'Human-Hero-3p':      'a3a,r1p,r1a,p3p,c3p',
-      'Human-Hero-3s':      'a3c,r1s,r1c,p3s,c3s',
+      'Human-Hero-3a':      'a3p,r1a,r1p,p2a,c3a:stealth',
+      'Human-Hero-3c':      'a3s,r1c,r1s,p2c,c3c:improve',
+      'Human-Hero-3p':      'a3a,r1p,r1a,p2p,c3p:upgrade',
+      'Human-Hero-3s':      'a3c,r1s,r1c,p2s,c3s:navi',
 
-      'Human-Hero-4a':      'a4p,r2a,r2p,p4a,c4a',
-      'Human-Hero-4c':      'a4s,r2c,r2s,p4c,c4c',
-      'Human-Hero-4p':      'a4a,r2p,r2a,p4p,c4p:mastery',
-      'Human-Hero-4s':      'a4c,r2s,r2c,p4s,c4s:mastery',
+      'Human-Hero-4a':      'a4p,r2a,r2p,p3a,c4a:stealth',
+      'Human-Hero-4c':      'a4s,r2c,r2s,p3c,c4c:heal+imp',
+      'Human-Hero-4p':      'a4a,r2p,r2a,p3p,c4p:fix+upg',
+      'Human-Hero-4s':      'a4c,r2s,r2c,p3s,c4s:navi',
 
       'Human-Hero-5a':      'a5s,r3a,r3s,p5a,c5a:mastery',
       'Human-Hero-5c':      'a5p,r3c,r3p,p5c,c5c',
@@ -68,17 +68,25 @@ class Human {
       'Human-Hero-6s':      'a6p,r4s,r4p,p6s,c6s:mastery',
 
 
-      'Human-Base-1a':      'a2a,p1a:recon',
+      'Human-Base-1a':      'a2a,p1a:mobilize',
+      'Human-Base-1c':      'a2c,p1c:bank',
       'Human-Base-1p':      'a2p,p1p:shipyard',
+      'Human-Base-1s':      'a2s,p1s:recycle',
 
-      'Human-Base-2a':      'a1a,p2a:recon,c2a',
-      'Human-Base-2p':      'a1p,p2p:shipyard,c2p',
+      'Human-Base-2a':      'a1a,c1a,p2a:mobilize',
+      'Human-Base-2c':      'a1c,c1c,p2c:bank',
+      'Human-Base-2p':      'a1p,c1p,p2p:shipyard',
+      'Human-Base-2s':      'a1s,c1s,p2s:recycle',
 
-      'Human-Base-3c':      'a4c,r1c,p3c:trade',
-      'Human-Base-3s':      'a4s,r1s,p3s:navi',
+      'Human-Base-3a':      'a4a,r2a,p3n:stealth',
+      'Human-Base-3c':      'a4c,r2c,p3c:heal+acad',
+      'Human-Base-3p':      'a4p,r2p,p3p:+load',
+      'Human-Base-3s':      'a4s,r2s,p3s:navi',
 
-      'Human-Base-4c':      'a3c,r2c,p4c:trade,c3c',
-      'Human-Base-4s':      'a3s,r2s,p4s:navi,c3s',
+      'Human-Base-4a':      'a3a,r2a,c3a,p4a:stealth',
+      'Human-Base-4c':      'a3c,r2c,c3c,p4c:heal+acad',
+      'Human-Base-4p':      'a3p,r2p,c3p,p4p:+++load',
+      'Human-Base-4s':      'a3s,r2s,c3s,p4s:navi',
 
       'Human-Base-5a':      'a6a,r3a,p3n:+takeoff',
       'Human-Base-5p':      'a6p,r3p,p3n:+draw',
@@ -87,17 +95,25 @@ class Human {
       'Human-Base-6p':      'a5p,r4p,p5p:+draw,c3p',
 
 
+      'Human-Colony-1a':    'a2a,p1a:recon',
       'Human-Colony-1c':    'a2c,p1c:academy',
-      'Human-Colony-1s':    'a2s,p1s:research',
+      'Human-Colony-1p':    'a2p,p1p:extract',
+      'Human-Colony-1s':    'a2s,p1s:explore',
 
-      'Human-Colony-2c':    'a1c,p2c:academy,c2c',
-      'Human-Colony-2s':    'a1s,p2s:research,c2s',
+      'Human-Colony-2a':    'a1a,c2a,p2a:recon',
+      'Human-Colony-2c':    'a1c,c2c,p2c:academy',
+      'Human-Colony-2p':    'a1p,c2p,p2p:extract',
+      'Human-Colony-2s':    'a1s,c2s,p2s:explore',
 
-      'Human-Colony-3a':    'a4a,r2a,p1a:mltrztn',
-      'Human-Colony-3p':    'a4p,r2p,p1p:extract',
+      'Human-Colony-3a':    'a4a,r2a,p1a:mobilize',
+      'Human-Colony-3c':    'a4c,r2c,p1c:export',
+      'Human-Colony-3p':    'a4p,r2p,p1p:fix+shipy',
+      'Human-Colony-3s':    'a4s,r2s,p1s:+range',
 
-      'Human-Colony-4a':    'a3a,r2a,p4a:mltrztn,c3a',
-      'Human-Colony-4p':    'a3p,r2p,p4p:extract,c3p',
+      'Human-Colony-4a':    'a3a,r2a,c3a,p4a:mobilize',
+      'Human-Colony-4c':    'a3c,r2c,c3c,p4c:export',
+      'Human-Colony-4p':    'a3p,r2p,c3p,p4p:fix+shipy',
+      'Human-Colony-4s':    'a3s,r2s,c3s,p4s:+++range',
 
       'Human-Colony-5c':    'a6c,r3c,p3c:-deplete',
       'Human-Colony-5s':    'a6s,r3s,p3s:+action',
