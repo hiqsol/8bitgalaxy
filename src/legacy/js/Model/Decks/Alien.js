ca45573442f30387cb96ae8aabeb7b4323eacd82
Andrii Vasyliev <sol@hiqdev.com>
2025-02-16 19:41:38 +0100

    js: init `Alien` deck

---
+++ src/legacy/js/Model/Decks/Alien.js
@@ -0,0 +1,12 @@
+class Alien {
+  static cards() {
+    return {
+      'Alien-Event-1p':     { Text: 'Each faction produces 2 materials' },
+      'Alien-Event-1c':     { Text: 'Each faction earns 2 money' },
+      'Alien-Event-1a':     { Text: 'Each faction builds ship (from factory)' },
+      'Alien-Event-1s':     { Text: 'Each faction explores 1 space' },
+    }
+  }
+}
+
+export default Alien;
